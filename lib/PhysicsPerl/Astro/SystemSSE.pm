8e5b136e1f48d880b31a5f90991ad20195bc1758
Will Braswell <william.braswell@autoparallel.com>
2017-01-25 08:37:30 -0600

    N-Body, Subclass SystemSSE From System

--- lib/PhysicsPerl/Astro/SystemSSE.pm
+++ lib/PhysicsPerl/Astro/SystemSSE.pm
@@ -7,8 +7,10 @@ use warnings;
 our $VERSION = 0.007_000;
 
 # [[[ OO INHERITANCE ]]]
-use parent qw(RPerl::CompileUnit::Module::Class);    # no non-system inheritance, only inherit from base class
-use RPerl::CompileUnit::Module::Class;
+#use parent qw(RPerl::CompileUnit::Module::Class);    # no non-system inheritance, only inherit from base class
+#use RPerl::CompileUnit::Module::Class;
+use parent qw(PhysicsPerl::Astro::System);
+use PhysicsPerl::Astro::System;
 
 # [[[ CRITICS ]]]
 ## no critic qw(ProhibitUselessNoCritic ProhibitMagicNumbers RequireCheckedSyscalls) # USER DEFAULT 1: allow numeric values & print operator
@@ -21,55 +23,10 @@ use PhysicsPerl::Astro::Body;
 use rperlsse;
 
 # [[[ OO PROPERTIES ]]]
-our hashref $properties = { bodies => my PhysicsPerl::Astro::Body_arrayref $TYPED_bodies->[5 - 1] = undef };
+#our hashref $properties = { bodies => my PhysicsPerl::Astro::Body_arrayref $TYPED_bodies->[5 - 1] = undef };
+our hashref $properties = {};
 
 # [[[ SUBROUTINES & OO METHODS ]]]
-
-our void::method $init = sub {
-    ( my PhysicsPerl::Astro::SystemSSE $self ) = @ARG;
-    $self->{bodies}->[0] = PhysicsPerl::Astro::Body::sun();
-    $self->{bodies}->[1] = PhysicsPerl::Astro::Body::jupiter();
-    $self->{bodies}->[2] = PhysicsPerl::Astro::Body::saturn();
-    $self->{bodies}->[3] = PhysicsPerl::Astro::Body::uranus();
-    $self->{bodies}->[4] = PhysicsPerl::Astro::Body::neptune();
-    my number $px = 0.0;
-    my number $py = 0.0;
-    my number $pz = 0.0;
-
-    for my integer $i ( 0 .. ( ( scalar @{ $self->{bodies} } ) - 1 ) ) {
-        $px += $self->{bodies}->[$i]->{vx} * $self->{bodies}->[$i]->{mass};
-        $py += $self->{bodies}->[$i]->{vy} * $self->{bodies}->[$i]->{mass};
-        $pz += $self->{bodies}->[$i]->{vz} * $self->{bodies}->[$i]->{mass};
-    }
-
-    $self->{bodies}->[0]->{vx} = -1 * ( $px / PhysicsPerl::Astro::Body::SOLAR_MASS() );
-    $self->{bodies}->[0]->{vy} = -1 * ( $py / PhysicsPerl::Astro::Body::SOLAR_MASS() );
-    $self->{bodies}->[0]->{vz} = -1 * ( $pz / PhysicsPerl::Astro::Body::SOLAR_MASS() );
-};
-
-our number::method $energy = sub {
-    ( my PhysicsPerl::Astro::SystemSSE $self ) = @ARG;
-    my number $dx;
-    my number $dy;
-    my number $dz;
-    my number $distance;
-    my number $e = 0.0;
-
-    for my integer $i ( 0 .. ( ( scalar @{ $self->{bodies} } ) - 1 ) ) {
-        my PhysicsPerl::Astro::Body_raw $body_i = $self->{bodies}->[$i]->get_raw();
-        $e += 0.5 * $body_i->{mass} * ( ( $body_i->{vx} * $body_i->{vx} ) + ( $body_i->{vy} * $body_i->{vy} ) + ( $body_i->{vz} * $body_i->{vz} ) );
-
-        for my integer $j ( ( $i + 1 ) .. ( ( scalar @{ $self->{bodies} } ) - 1 ) ) {
-            my PhysicsPerl::Astro::Body_raw $body_j = $self->{bodies}->[$j]->get_raw();
-            $dx       = $body_i->{x} - $body_j->{x};
-            $dy       = $body_i->{y} - $body_j->{y};
-            $dz       = $body_i->{z} - $body_j->{z};
-            $distance = ( ( $dx * $dx ) + ( $dy * $dy ) + ( $dz * $dz ) )**0.5;
-            $e -= ( $body_i->{mass} * $body_j->{mass} ) / $distance;
-        }
-    }
-    return $e;
-};
 
 our void::method $advance_loop = sub {
     ( my PhysicsPerl::Astro::SystemSSE $self, my constant_number $delta_time, my constant_integer $time_step_max ) = @ARG;
